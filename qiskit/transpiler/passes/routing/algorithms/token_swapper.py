0bfe08d4ad6fdb406092f77316372029801370fe
Toshinari Itoko <itoko@jp.ibm.com>
2020-02-10 16:10:01 +0900

    put algorithms into one file algorithms/token_swapper.py

--- qiskit/transpiler/passes/routing/algorithms/token_swapper.py
+++ qiskit/transpiler/passes/routing/algorithms/token_swapper.py
@@ -30,15 +30,25 @@
 
 import logging
 from typing import TypeVar, Iterator, Mapping, Generic, MutableMapping, MutableSet, List, \
-    Iterable, Optional, Union
+    Iterable, Optional, Union, Dict, Tuple, NamedTuple, Any
 
 import networkx as nx
 import numpy as np
 
-from qiskit.transpiler.routing import Swap
+from qiskit.circuit import QuantumRegister
+from qiskit.dagcircuit import DAGCircuit
+from qiskit.extensions import SwapGate
 
+# Type definitions used within the permutation package.
+PermuteElement = TypeVar('PermuteElement')
+Permutation = Dict[PermuteElement, PermuteElement]
+Swap = Tuple[PermuteElement, PermuteElement]
+
+_K = TypeVar('_K')
 _V = TypeVar('_V')
 _T = TypeVar('_T')
+Reg = Tuple[str, int]
+
 
 logger = logging.getLogger(__name__)
 
@@ -231,3 +241,75 @@ class ApproximateTokenSwapper(Generic[_V]):
         else:
             sequential_swaps = self.map(mapping)
         return [[swap] for swap in sequential_swaps]
+
+
+# Represents a circuit for permuting to a mapping with the associated cost.
+PermutationCircuit = NamedTuple('PermutationCircuit',
+                                [('circuit', DAGCircuit),
+                                 ('inputmap', Dict[Any, Reg])
+                                 # A mapping from architecture nodes to circuit registers.
+                                 ])
+
+
+def swaps_to_permutation_circuit(swaps: Iterable[List[Swap[_V]]]) -> PermutationCircuit:
+    """Produce a circuit description of a list of swaps.
+        With a given permutation and permuter you can compute the swaps using the permuter function
+        then feed it into this circuit function to obtain a circuit description.
+    Args:
+      swaps: An iterable of swaps to perform.
+    Returns:
+      A MappingCircuit with the circuit and a mapping of node to qubit in the circuit.
+    """
+    # Construct a circuit with each unique node id becoming a quantum register of size 1.
+    dag = DAGCircuit()
+    swap_list = list(swaps)
+
+    # Set of unique nodes used in the swaps.
+    nodes = {
+        swap_node
+        for swap_step in swap_list
+        for swap_nodes in swap_step
+        for swap_node in swap_nodes
+    }
+
+    node_qargs = {node: QuantumRegister(1) for node in nodes}
+    for qubit in node_qargs.values():
+        dag.add_qreg(qubit)
+
+    inputmap = {node: q[0] for node, q in node_qargs.items()}
+
+    # Apply swaps to the circuit.
+    for swap_step in swap_list:
+        for swap0, swap1 in swap_step:
+            dag.apply_operation_back(SwapGate(), [inputmap[swap0], inputmap[swap1]])
+
+    return PermutationCircuit(dag, inputmap)
+
+
+def swap_permutation(swaps: Iterable[Iterable[Swap[_K]]],
+                     mapping: MutableMapping[_K, _V],
+                     allow_missing_keys: bool = False) -> None:
+    """Given a circuit of swaps, apply them to the permutation (in-place).
+
+    Args:
+      swaps: param mapping: A mapping of Keys to Values, where the Keys are being swapped.
+      mapping: The permutation to have swaps applied to.
+      allow_missing_keys: Whether to allow swaps of missing keys in mapping.
+    """
+    for swap_step in swaps:
+        for sw1, sw2 in swap_step:
+            # Take into account non-existent keys.
+            val1 = None  # type: Optional[_V]
+            val2 = None  # type: Optional[_V]
+            if allow_missing_keys:
+                val1 = mapping.pop(sw1, None)
+                val2 = mapping.pop(sw2, None)
+            else:
+                # Asserts that both keys exist
+                val1, val2 = mapping.pop(sw1), mapping.pop(sw2)
+
+            if val1 is not None:
+                mapping[sw2] = val1
+            if val2 is not None:
+                mapping[sw1] = val2
+
